7cd36989c9e4f708071fdf95b70aa7db6c7ea0bc
kalle saas <kalle@easypep.de>
2021-02-16 15:11:23 +0100

    always call jsonapi_page_size method on pagination enabled requests (#40)
    * always call jsonapi_page_size method on pagination enabled requests
    
    pass the per_page_param to the jsonapi_page_size method
    
    * pass pagination to jsonapi_page_size and handle string-to-int conversion in jsonapi_page_size
    
    * update readme to reflect changes
    
    * fix woring

--- lib/jsonapi/pagination.rb
+++ lib/jsonapi/pagination.rb
@@ -96,8 +96,7 @@ module JSONAPI
     # @return [Array] with the offset, limit and the current page number
     def jsonapi_pagination_params
       pagination = params[:page].try(:slice, :number, :size) || {}
-      per_page = pagination[:size].to_f.to_i
-      per_page = jsonapi_page_size if per_page < 1
+      per_page = jsonapi_page_size(pagination)
       num = [1, pagination[:number].to_f.to_i].max
 
       [(num - 1) * per_page, per_page, num]
@@ -105,9 +104,19 @@ module JSONAPI
 
     # Retrieves the default page size
     #
+    # @param per_page_param [Hash] opts the paginations params
+    # @option opts [String] :number the page number requested
+    # @option opts [String] :size the page size requested
+    #
     # @return [Integer]
-    def jsonapi_page_size
-      self.class.const_get(:JSONAPI_PAGE_SIZE).to_i
+    def jsonapi_page_size(pagination_params)
+      per_page = pagination_params[:size].to_f.to_i
+
+      return self.class
+              .const_get(:JSONAPI_PAGE_SIZE)
+              .to_i if per_page < 1
+
+      per_page
     end
 
     # Fallback to Rack's parsed query string when Rails is not available
