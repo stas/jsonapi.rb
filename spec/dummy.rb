d254f2bffb41540bb683d18607e7b1f210f5e1eb
Martin Meyerhoff <mamhoff@gmail.com>
2024-01-31 16:55:38 +0100

    Dummy App: Add Ransack safelists
    Ransack 4 requires safelists for ransackable attributes on all ransacked
    models. This PR adds them, makind the tests run again.

--- spec/dummy.rb
+++ spec/dummy.rb
@@ -30,12 +30,28 @@ end
 
 class User < ActiveRecord::Base
   has_many :notes
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["last_name", "first_name", "created_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["notes"]
+  end
 end
 
 class Note < ActiveRecord::Base
   validates_format_of :title, without: /BAD_TITLE/
   validates_numericality_of :quantity, less_than: 100, if: :quantity?
   belongs_to :user, required: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["quantity", "created_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["user"]
+  end
 end
 
 class CustomNoteSerializer
